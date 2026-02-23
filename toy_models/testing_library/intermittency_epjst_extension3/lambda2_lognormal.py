7693deeb56f41be879446487e0af8acbd8871993
Filip Chudzynski <filipchudzynski@gmail.com>
2026-02-23 20:42:37 +0000

    https://github.com/filipchudzynski/stock-market-non-gaussianity-analyzer_v2/issues/15

--- toy_models/testing_library/intermittency_epjst_extension3/lambda2_lognormal.py
+++ toy_models/testing_library/intermittency_epjst_extension3/lambda2_lognormal.py
@@ -5,3 +5,9 @@ def estimate_lambda2_lognormal(inc):
     inc=np.abs(inc); inc=inc[inc>0]
     if len(inc)<MIN_SAMPLES: return np.nan
     return np.var(np.log(inc),ddof=1)
+
+def lambda2_lognormal_estimator(inc):
+    Z = np.array(inc)
+    Y = np.log(Z**2)
+    lambda2 = (0.25 * np.var(Y, ddof=1))  # ddof=1 = unbiased
+    return lambda2
